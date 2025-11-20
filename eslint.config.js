ec88356d4c72cfddf51d05a6ea9dd06eb0c4e34e
Hal Shin <hal@bandh.ca>
2025-11-20 11:58:03 -0800

    Fix eslint config for env/globals

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,13 @@ import js from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default [
     js.configs.recommended,
     {
-        env: {
-            node: true,
+        globals: {
+            ...globals.node,
         },
         files: ['**/*.ts', '**/*.tsx'],
         languageOptions: {
